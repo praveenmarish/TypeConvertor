2b215679ed03033951440614e9805dc820835254
praveenmarish <praveenmarish0059@gmail.com>
2022-05-03 16:32:31 +0530

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,37 @@
+var a = [
+  {
+    name: 'userName',
+    value: 'Marish',
+  },
+  {
+    name: 'age',
+    value: '12',
+  },
+  {
+    name: 'lang',
+    value: 'eng',
+  },
+  {
+    name: 'lang',
+    value: 'tam',
+  },
+  {
+    name: 'lang',
+    value: 'hin',
+  },
+];
 
+var res = {};
+
+a.map((item, _i) => {
+  if (res[item.name]) {
+    res[item.name] =
+      typeof res[item.name] == 'string'
+        ? [res[item.name], item.value]
+        : [...res[item.name], item.value];
+  } else {
+    res[item.name] = item.value;
+  }
+});
+
+console.log(a, res);
